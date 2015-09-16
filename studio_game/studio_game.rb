dea58c5fc9ac5dcfd6de8cfdea0da1b4d142082d
Harish Ramachandran <harishsr@beardendesigns.com>
2015-09-15 21:50:55 -0400

    3.2

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,7 +1,7 @@
-greeting = "Welcome!"
+name1 = "larry"
+name2 = "curly"
+name3 = "moe"
+health1 = 60
+puts "#{name1}'s health is #{health1 / 9.0}"
 
-3.times do 
-  puts greeting.upcase
-end
-
-puts Time.now
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
