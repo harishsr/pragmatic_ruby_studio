013f54bf0bfc6257f8895468cf030b0f1f5f250f
Harish Ramachandran <harishsr.coder@gmail.com>
2015-09-30 22:05:49 -0400

    14

--- studio_game/game.rb
+++ studio_game/game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'game_turn'
 
 class Game
   attr_reader :title
@@ -19,16 +20,7 @@ class Game
       puts player
     end
     @players.each do |player|
-      die = Die.new
-      case die.roll
-      when 1..2
-        player.blam
-      when 3..4
-        puts "#{player} was skipped."
-      when 5..6
-        player.w00t
-      end
-      puts player
+      GameTurn.take_turn(player)
     end
   end
 end
