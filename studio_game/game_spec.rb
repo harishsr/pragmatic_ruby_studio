1908bbc38b91f7e29fa89be502d3c99a422acd0c
Harish Ramachandran <harishsr.coder@gmail.com>
2015-09-30 22:34:39 -0400

    pending...

--- studio_game/game_spec.rb
+++ studio_game/game_spec.rb
@@ -12,20 +12,20 @@ describe Game do
   context "a high number" do 
     it "w00ts a player if a high number is called" do 
       allow_any_instance_of(Die).to receive(:roll).and_return(5)
-      @game.play
-      expect(@player.health).to eq(@initial_health + 15)
+      @game.play(2)
+      expect(@player.health).to eq(@initial_health + (15 * 2))
     end
 
     it "skips a player if a medium number is called" do 
       allow_any_instance_of(Die).to receive(:roll).and_return(3)
-      @game.play
+      @game.play(2)
       expect(@player.health).to eq(@initial_health)
     end
 
     it "blams a player if a low number is called" do 
       allow_any_instance_of(Die).to receive(:roll).and_return(1)
-      @game.play
-      expect(@player.health).to eq(@initial_health - 10)
+      @game.play(2)
+      expect(@player.health).to eq(@initial_health - (10 * 2))
     end
   end
 end
